5d950d742c9c6791a3fe4a8f7376f3be3461faad
marilyn pagan <mpagan6@gmail.com>
2018-02-06 15:50:33 -0600

    "added images, updated files"

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// Player has to guess the number
+// 4 crystals need to be displayed as buttons
+// crystal values 1-12
+// Players are shown a random number at the beginning of the game
+// Players will click on button to gain points 
+// points gained are shown after click and counter is updated
+// player wins if they match random initial number 
+// player loses if they go above the random number
+// game restarts whenever player wins or loses
+// new random number when game restarts
+// game will keep track of wins and losses
+// random number shown at the start of game between 19-120
+
+var crystals = on.click 
