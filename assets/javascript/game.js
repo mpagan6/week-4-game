1bf4ef9fb13d1277a2b0bac0fdf0ca2d16ed9584
marilyn pagan <mpagan6@gmail.com>
2018-02-06 18:06:55 -0600

    "updates"

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,4 +11,75 @@
 // game will keep track of wins and losses
 // random number shown at the start of game between 19-120
 
-var crystals = on.click 
+//create variables
+//random between 19-120 
+    var randomNumber= Math.floor((Math.random * 120) + 1);
+
+ //Crystals random values 1-12
+   var blue = Math.floor((Math.random * 12) + 1);
+   var green = Math.floor((Math.random * 12) + 1);
+   var red = Math.floor((Math.random * 12) + 1);
+   var yellow = Math.floor((Math.random * 12) + 1);
+
+   var wins = 0;
+   var losses = 0;
+   var totScore= 0; 
+
+   $(document).ready(function() {
+        
+    $("#ranNumber").text(randomNumber);
+       
+
+        $(".blue").click(function() {
+        update(blue);
+        });
+      
+        $(".green").click(function() {
+        update(green);
+        });
+      
+        $(".red").click(function() {
+        update(red);
+        });
+      
+        $(".yellow").click(function() {
+        update(yellow);
+        });
+      });
+      
+      
+      
+      function reset() {
+        randomNumber = Math.floor((Math.random() * 120) + 19);
+        $(".ranNumber").html(randomNumber);
+      
+        blue = Math.floor((Math.random() * 12) + 1);
+        green = Math.floor((Math.random() * 12) + 1);
+        red = Math.floor((Math.random() * 12) + 1);
+        yellow = Math.floor((Math.random() * 12) + 1);
+      
+        totScore = 0;
+      
+        $(".current").html(totScore);
+      }
+      
+      
+      function update(color) {
+        totScore += color;
+      
+        $(".current").empty();
+        $(".current").append(totScore);
+      
+        if (totScore > ranNumber) {
+          losses++;
+          $(".losses").html(losses);
+      
+          reset();
+      
+        } else if (totScore == ranNumber) {
+            wins++;
+            $(".wins").html(wins);
+            reset();
+        };
+      };
+
